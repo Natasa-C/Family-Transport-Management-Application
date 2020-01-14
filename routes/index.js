5c9de07187461c7da528828c77a0313574b1844d
Natasa_C <57111995+Natasa-C@users.noreply.github.com>
2020-01-14 18:33:42 +0200

    initial setup

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = {};
+
+router.get('/', function(req, res) {
+    res.render('index');
+})
+
+
+module.exports.router = router;
