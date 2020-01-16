ffd49133e506e5700f392931635ae57585920ccb
Natasa_C <57111995+Natasa-C@users.noreply.github.com>
2020-01-16 18:21:04 +0200

    version 3.0.0

--- routes/transport.js
+++ routes/transport.js
@@ -3,68 +3,79 @@ const express = require('express');
 const router = express.Router();
 const BoxInfo = require('../models/transport');
 const fs = require('fs');
-const path = '/home/natasa/Personal/Facultate/TEHNICIWEB/FamilyApp/public/transport_data.json'
-module.exports = {};
+const async = require('async');
+const path = '/home/natasa/Personal/Facultate/TEHNICIWEB/FamilyApp/public/transport_data.json';
 
-// All transport boxes Route
-router.get('/', function(req, res) {
-    res.render('transports/index');
-})
 
-// New transport boxes Route
-router.get('/new', function(req, res) {
-    res.render('transports/new', { boxInfo: new BoxInfo() });
-})
+module.exports = function transportFunc(app, collection) {
+    // All transport boxes Route
+    router.get('/', async function(req, res) {
+        let data;
 
-// Create Box Route
-router.post('/', function(req, res) {
-    const boxInfo = {
-        departure: req.body.departure,
-        arrival: req.body.arrival,
-        date: req.body.date,
-        time: req.body.time,
-        from_location: req.body.from_location,
-        to_location: req.body.to_location,
-        number_of_sits: req.body.number_of_sits,
-    }
-
-    if (fs.existsSync(path)) {
-        var obj = fs.readFileSync(path, function(err, data) {
-            if (err) {
-                res.render('transports/page_not_found');
-            }
+        let prom = new Promise((resolve, reject) => {
+            app.db[collection].find({}).toArray()
+                .then(result => {
+                    if (result) {
+                        data = result;
+                    } else {
+                        data = -1;
+                    }
+                    resolve(data);
+                })
+                .catch(function(error) {
+                    console.error(`Failed to find document: ${error}`);
+                    reject();
+                })
         });
-        var vector = JSON.parse(obj);
-        vector.push(boxInfo);
-        var text = JSON.stringify(vector);
-        fs.writeFileSync(path, text);
-    }
+        await prom;
 
-    res.render('transports/submitted')
+        res.render('transports/index', { info: data });
+    })
 
-    // const boxInfo = new BoxInfo({
-    //     departure: req.body.departure,
-    //     arrival: req.body.arrival,
-    //     date: req.body.date,
-    //     time: req.body.time,
-    //     from_location: req.body.from_location,
-    //     to_location: req.body.to_location,
-    //     number_of_sits: req.body.number_of_sits,
-    // })
+    // New transport boxes Route
+    router.get('/new', function(req, res) {
+        res.render('transports/new', { boxInfo: new BoxInfo() });
+    })
 
-    // boxInfo.save((error, boxInfo) => {
-    //     if (error) {
-    //         res.render('transports/page_not_found', {
-    //             boxInfo: boxInfo,
-    //             errorMessage: 'Error Creating Transport'
-    //         })
-    //     } else {
-    //         res.redirect('transports');
-    //         // res.redirect(`transports/${boxInfo.id}`
-    //     }
-    // })
-})
+    // Create Box Route
+    router.post('/', async function(req, res) {
+        const boxInfo = {
+            departure: req.body.departure,
+            arrival: req.body.arrival,
+            date: req.body.date,
+            time: req.body.time,
+            from_location: req.body.from_location,
+            to_location: req.body.to_location,
+            number_of_sits: req.body.number_of_sits,
+            comeing: ''
+        }
 
+        if (fs.existsSync(path)) {
+            var obj = fs.readFileSync(path, function(err, data) {
+                if (err) {
+                    res.render('transports/page_not_found');
+                }
+            });
+            var vector = JSON.parse(obj);
+            vector.push(boxInfo);
+            var text = JSON.stringify(vector);
+            fs.writeFileSync(path, text);
+        }
 
+        let prom = new Promise((resolve, reject) => {
+            app.db[collection].insertOne(boxInfo)
+                .then(result => {
+                    resolve();
+                })
+                .catch(function(error) {
+                    console.error(`Failed to insert item: ${error}`);
+                    reject();
+                })
+        });
+        await prom;
 
-module.exports.router = router;
+        res.render('transports/submitted')
+    })
+
+    return router;
+}
--- server.js
+++ server.js
@@ -2,16 +2,83 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const async = require('async');
+
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.mongo_host;
+
+const database_name = process.env.database_name;
+const transport_data_collection = process.env.transport_data_collection;
 
 const indexRouter = require('./routes/index').router;
-const transportRouter = require('./routes/transport').router;
+const transportRouterFunc = require('./routes/transport');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
-app.use('/', indexRouter);
-app.use('/transports', transportRouter);
 
-app.listen(process.env.PORT);
+async.auto({
+    mongo_connect: async function() {
+        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        try {
+            await client.connect();
+            console.log('Successfully connected to database!');
+        } catch (error) {
+            console.error('Error occured while connecting to database: ', error);
+            return 0;
+        }
+
+        app.db = await client.db(database_name);
+        app.client = await client;
+        app.db[transport_data_collection] = await app.db.collection(transport_data_collection);
+        app.db.config = await app.db.collection("config");
+
+        // try {
+        //     await app.db[transport_data_collection].createIndex({ id: 1 });
+        //     console.log('Successfully created index on id field.');
+
+        // }
+        // catch (error) {
+        //     console.error(`Failed to create index: ${error}`);
+        //     return 0;
+        // }
+
+        return 1;
+    },
+
+    route_manager: ['mongo_connect', async function(results) {
+        if (results.mongo_connect == 0)
+            return 0;
+
+        app.use('/', indexRouter);
+        app.use('/transports', transportRouterFunc(app, transport_data_collection));
+
+        return 1;
+    }],
+
+    start: ['route_manager', async function(results) {
+        if (results.route_manager == 0)
+            return 0;
+
+        app.listen(process.env.PORT);
+
+        return 1;
+    }]
+
+    // mongo_close: ['start', async function(results) {
+    //     if (results.mongo_connect != 0) {
+    //         if (app.client != undefined) {
+    //             await app.client.close();
+    //         }
+    //         console.log('Successfully closed the database!');
+    //     }
+    //     return 1;
+    // }],
+
+}, function(err, results) {
+    console.log('err = ', err);
+    console.log('Finished everything!');
+});
