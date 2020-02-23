3f99d32e4d555479a3ed2af962ced84677f903f6
Natasa_C <57111995+Natasa-C@users.noreply.github.com>
2020-02-23 12:07:51 +0200

    utility_functions.js - changed some functions

--- public/utility_functions.js
+++ public/utility_functions.js
@@ -5,18 +5,26 @@ window.onload = function() {
     var delete_meet_content = document.getElementById("delete-meet-content");
     var delete_el = document.getElementById("first-meet");
 
+    document.getElementById("coordinates").onclick = Poz;
+
+    function Poz(event) {
+        document.getElementById("coordinates").innerText = "Coordinates:\nevent.clientX-Y " + event.clientX + ' ' + event.clientY + '\n' +
+            "event.pageX-Y: " + event.pageX + ' ' + event.pageY + '\n' +
+            "event.screenX-Y " + event.screenX + ' ' + event.screenY;
+    }
+
     var data;
     var httpObj;
 
     var interval;
-    var end = 15000;
+    // var end = 15000;
 
     function startInterval() { interval = setInterval(make_request, 3000); }
 
-    function stopInterval() { clearInterval(interval); }
+    // function stopInterval() { clearInterval(interval); }
 
     startInterval();
-    setTimeout(stopInterval, end);
+    // setTimeout(stopInterval, end);
 
     slideshow.onclick = function() {
         make_request();
@@ -28,13 +36,12 @@ window.onload = function() {
 
 
     open_window.onclick = function() {
-        myWindow = window.open("https://www.shutterfly.com/ideas/family-quotes/");
+        myWindow = window.open("https://www.shutterfly.com/ideas/family-quotes/", "_blank", "top=100, left=200, height=400, width=900, toolbar=no");
     }
 
     close_window.onclick = function() {
         myWindow.close();
     }
-    alert("ooooo111")
 
     add_meet_content.onclick = function() {
         var obj = document.createElement("article");
@@ -79,8 +86,7 @@ function change_picture(data) {
 
     if (httpRequest.readyState === 4) {
         if (httpRequest.status === 200) {
-
-            if (index == NaN) {
+            if (index == 'NaN') {
                 index = 0;
                 localStorage.setItem("slideshowPictureNumber", "0");
 
